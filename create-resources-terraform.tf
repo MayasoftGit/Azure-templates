dc493f6f8f0b9e39478c46f38a0cc483a1d02326
MayasoftGit <github@mayasoft.com.tr>
2024-11-27 09:49:11 +0300

    Create create-resources-terraform.tf

---
+++ create-resources-terraform.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_virtual_machine" "example" {
+  name                  = "example-vm"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  vm_size               = "Standard_B1s"
+
+  os_profile = {
+    computer_name  = "examplevm"
+    admin_username = "adminuser"
+    admin_password = "ComplexPassword123!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
